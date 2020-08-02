5a905310db7e1e1f76b7227c483187945a4bcd69
Nicholas <nicxzmiller2@gmail.com>
2020-08-03 00:20:56 +0200

    changing input design

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,10 +4,11 @@ import TodoItem from "./TodoItem";
 class TodoList extends Component {
     render() {
         return (
-            <div>
-                <h1>Hello</h1>
+           <ul className="list-group my-5">
+               <h3 className="text-capitalize text-center">Todo List</h3>
                <TodoItem/>
-            </div>
+               <button type="button" className="btn btn-block btn-danger mt-3 shadow-none text-capitalize">Clear List</button>
+           </ul>
         );
     }
 }
