d5ff805bede8bc32e11edda517edbaf4831c6c00
Nicholas <nicxzmiller2@gmail.com>
2020-08-02 17:52:04 +0200

    firts commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import './Header';
+import Header from "./Header";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    state = {
+        tasks: ['task 1', 'task 2', 'task 3']
+    };
+    render(){
+        return (
+            <div className="App">
+                <h1> TodoList </h1>
+                <Header numTodos={this.state.tasks.length}/>
+            </div>
+        );
+    }
 }
 
 export default App;
